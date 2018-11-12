a75b4808c6bb39ffac8a377ac90fd60b3fe97f5a
SunShield <ustyanherman@gmail.com>
2018-11-12 10:37:52 +0300

    Added forgotten file.

---
+++ TestTasks/TestTask/Assets/Code/Scripts/Cylinder/StartingCylinderController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace MyGame.Cylinder
+{
+	class StartingCylinderController : CylinderController
+	{
+		private readonly Vector3 StartingCylinderDefaultScale = new Vector3(1f, 0.25f, 1f);
+
+		protected override Vector3 GetDefaultScale()
+		{
+			return StartingCylinderDefaultScale;
+		}
+	}
+}
